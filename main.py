0fa7020b0fe4b2fdbe5897ff49c6b8735fac0fd0
svenjadunger <svenjadunger@web.de>
2024-10-28 20:33:13 +0100

    lists, negative indexes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+print('Hello World')
+message = "hello world"
+print(len(message))
+print(message[0])
+print(message[0:5])
+print(message.lower())
+print(message.upper())
 
-num_1 = '3'
-num_2 = '500'
-
-num_1 = int(num_1)
-num_2 = int(num_2)
-
-print(num_1 + num_2)
