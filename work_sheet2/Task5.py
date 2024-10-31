43a63bb77b41aa4404a4d8a35378a5d8a03a0f54
svenjadunger <svenjadunger@web.de>
2024-10-31 14:16:01 +0100

    task: Variables and data types

---
+++ work_sheet2/Task5.py
@@ -0,0 +1,13 @@
+var_int = 5
+var_float = 3.5
+var_str = "Programmieren"
+var_bool = True
+var_list = ['Stuhl', 'Tisch']
+var_tupl = ('Berlin', 'Hamburg', 96688)
+
+result = var_int + var_float
+
+end_of_list = var_list.pop()
+
+print(var_list)
+print(end_of_list)
