dbfabbb6f20441b3e562be0d400e6677a6692db0
svenjadunger <svenjadunger@web.de>
2024-10-31 13:11:02 +0100

    task: translate the quadratic formula to python code

---
+++ work_sheet2/Task2.py
@@ -0,0 +1,8 @@
+string1 = "This is a wuggie. Now there are two wuggies."
+print(string1[19])
+print(string1[0:3])
+print(string1[0] + string1[2] + string1[10] + string1[15] + string1[10] + string1[25] + string1[15], string1[2])
+print(string1[3] + string1[2] + string1[1] + string1[0])
+# print(string1[0:4][::-1])
+print(string1[::-1])
+
