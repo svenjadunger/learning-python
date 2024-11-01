fc67c0c215e58c7e387226735cb252dc1def98fa
svenjadunger <svenjadunger@web.de>
2024-11-01 20:09:11 +0100

    chrore: improve error message

--- work_sheet2/Task1.py
+++ work_sheet2/Task1.py
@@ -4,14 +4,14 @@ a = float(input("Whats number a? "))
 b = float(input("Whats number b? "))
 c = float(input("Whats number c? "))
 
-print(f"Number a is: {a}")
-print(f"Number b is: {b}")
-print(f"Number c is: {c}")
+
 
 if (b**2 - 4 * a * c) > 0:
     x1 = round((-b - sqrt(b**2 - 4 * a * c)) / (2*a), 4)
     x2 = round((-b + sqrt(b**2 - 4 * a * c)) / (2*a), 4)
     print(f"The result for x1 is: {x1}")
     print(f"The result for x2 is: {x2}")
+else:
+    print(f"ERROR: The equation under the square root is to small \n[got: {b**2 - 4 * a * c}]  [want: >0] \n ")
 
 
