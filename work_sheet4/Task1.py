cc64afced09710e8b2d7f8c1f8bac93ec284b5e0
svenjadunger <svenjadunger@web.de>
2024-11-11 20:36:15 +0100

    feat: complete task 1 and 2

---
+++ work_sheet4/Task1.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# Group I
+# Matriculation numbers: [827575, 826703, 828610]
+# Sheet 4, Task 1
+
+
+import random
+
+# create needed variables
+options = ["scissors", "paper", "rock"]
+
+print("Let's play rock, paper, scissors!")
+
+player_b = input("Please input one of the following: " + "scissors, paper, rock" + "\n")
+
+# an element from the list 'options' is selected randomly
+player_a = random.choice(options)
+
+
+
+# validate user input
+if player_b not in options:
+    print("Please choose one of the given options!")
+else:
+    print("vs.\n" + player_a)
+    print("-"*25)
+    if player_a == player_b:
+        print("There is no winner!")
+    elif player_b == "scissors" and player_a == "paper":
+        print("You won!")
+    elif player_b == "paper" and player_a == "rock":
+        print("You won!")
+    elif player_b == "rock" and player_a == "scissors":
+        print("You won!")
+    else:
+        print("You lost!")
---
+++ work_sheet4/Task2.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# Group I
+# Matriculation numbers: [827575, 826703, 828610]
+# Sheet 4, Task 1
+
+contaminated_token = input("Enter a token: ")
+contaminated_token = contaminated_token.strip()
+
+if contaminated_token == "":
+    print("No token found.")
+elif (contaminated_token.startswith('"') and contaminated_token.endswith('"')) or \
+     (contaminated_token.startswith("'") and contaminated_token.endswith("'") and not "'s" in contaminated_token):
+    cleaned_token = contaminated_token[1:-1] #removes last and first Zeichen
+    print(cleaned_token)
+elif "'s" in contaminated_token:
+    print(contaminated_token)
+elif contaminated_token in [".", ",", "!", "?"]:
+    print("No token found.")
+else:
+    cleaned_token = contaminated_token.replace(",", "").replace(".", "").replace("!", "").replace("?", "")
+    print(cleaned_token)
