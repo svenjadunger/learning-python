c34570afbd715b7ef8a448314d3a2a4bfc6ba27e
svenjadunger <svenjadunger@web.de>
2024-11-11 18:00:59 +0100

    add notes

---
+++ work_sheet3/algorithms.py
@@ -0,0 +1,62 @@
+# parts computer
+# brain = cpu - pieces hardware, respond to instructions
+# memory is RAM - where your game is stored while its being used, 
+# algorithm list of instructions to tell people what to do (step by step instructions)
+# 
+
+#flowchart, flow diagram, pseudocode
+
+#flowchart graphical representation of an algorithm
+#uses formally standard symbols connected by lines with arrows to show the flow
+
+
+#starting point or ending point -  start
+#parallelogramm input or output from the process with the algorithm zb input 
+#4eck process that is performed within the algorithm  zb cashback = 0 set it to 0
+#route indicaes selection or decesion within the algorithn zb if purchcadr > 500
+# -> locacial flow algorithm
+
+#Beispiel: Algorithm to add two numbers 10 and 20
+# number 1 = 10, number 2 = 20
+#add number 1 and number 2
+
+#kreis Start -> 4eck number 1=10 -> 4eck number 2= 20 -> add number 1 and number 2 -> parralelogram Output answer -> kreis End
+# kreis start -> parallelogramm input number 1 -> parall. input number 2 -> route is number 1 >= 2 -> pfeil nach li. true, pfeil na. re. false -> v
+# von pfeil true -> substract number 2 from number 1 -> pfeil von false: subtract number 1 from number2
+# pfeile gehen zusammen -> 4eck store answer as number 3 -> parallel. ourtput number 3 -> kreis End
+
+
+#Pseudocode
+
+# resemles closely to programming language but is not a programming language
+# doesnt run on a computer, needs to be converted in a programming language
+#different variations, should be easy to follow
+
+
+#Beispiel: Algorithm to add two numbers 10 and 20
+# number 1 = 10, number 2 = 20
+#add number 1 and number 2
+
+
+#vartiable is named as a single word
+# FirstName zb name oder first_Name
+# number1 <-10 or number1= 10 or SET number1 to 10
+#answer <- number1+ number 2 or answer = number1 + number2 or SET answer to SUM of number1 and number2
+# OUTPUT answer or SEND answer to DISPLAY
+#
+
+#IF THEN ELSE STATEMENTS
+
+# number 1 <- USERINPUT
+# number2 <- USERINPUT
+# IF number1 >= number 2 THEN
+# answer <- number1 - number2
+# ELSE
+#answer <- number 2-number1
+#ENDIF 
+# number3 <- answer
+#OUTPUT number3
+#
+
+# writing pseudocode before writing acutal code 
+
