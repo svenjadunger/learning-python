03f0a89d1697315e436acc7913cac93f4151fba1
svenjadunger <svenjadunger@web.de>
2024-11-24 17:31:37 +0100

    feat: complete task 4

--- work_sheet5/Task4.py
+++ work_sheet5/Task4.py
@@ -4,14 +4,11 @@
 # Matriculation numbers: [827575, 826703, 828610]
 # Sheet 5, Task 4
 
-n = int(input("Please enter a number of stars (maximum):  ")) #zb 5
+n = int(input("Please enter a number of stars (maximum):  "))
 
-# aufsteigend (1,6)
-for i in range(1, n + 1): #i=1, ... i=5
-    print('*' * i) #'*' * 1 = *,.... '*' * 5 = *****
-
-# absteigend (4, 0, -1) ende bei 1 (1 exklusive, n. gezählt)
-for i in range(n - 1, 0, -1): #i=4, i=3,.. i=1
-    print('*' * i) # '*' * 4 = ****,..... *
+for i in range(1, n + 1):
+    print('*' * i)
+for i in range(n - 1, 0, -1):
+    print('*' * i)
     
     
