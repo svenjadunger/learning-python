f34bfeb0b86b79139504d865e2e5fd5f9a4f13b1
svenjadunger <svenjadunger@web.de>
2024-12-12 21:29:25 +0100

    feat: add some comments

--- work_sheet6/Task2.py
+++ work_sheet6/Task2.py
@@ -7,17 +7,13 @@
 
 def is_prime(number):
 
-    value_prime = True
+    if number <= 1: return False
 
-    if number > 1:
+    for num in range(2, int(number ** 0.5) + 1):
+        if number % num == 0:
+            return False
+    return True
 
-        for num in range(2, int(number ** 0.5) + 1):
-            if number % num == 0:
-                value_prime = False
-    else:
-        value_prime = False
-
-    return value_prime
 
 user_number = int(input("Enter a number: "))
 print(f"Is the number {user_number} prime? {is_prime(user_number)}")
--- work_sheet7/Task2.py
+++ work_sheet7/Task2.py
@@ -5,8 +5,8 @@
 # Sheet 7, Task 2
 
 def node_count(tree):
-    if isinstance(tree[1], str):  # ist zweites element ein string? wenn ja: Blattknoten, zb the oder cat
-        return 0 #0 zurückgeb, da blattknoten keine nicht-blattknoten sind
+    if isinstance(tree[1], str):  # ist zweites element tree[1] ein string? wenn ja: Blattknoten, zb the oder cat
+        return 0 # ...wenn ja dann 0 zurückgeb, da blattknoten keine nicht-blattknoten sind
     return 1 + sum(node_count(child) for child in tree[1])  #falls 2. element eine liste,
 #ist es ein Nicht-blattknoten (zb S oder NP), +1 um aktuellen knoten zu zählen
 #f. jeden teilbaum(child) i.d. liste tree[1] wird funkt. node.c. rekurisv aufgerufen, ergebnisse summiert
