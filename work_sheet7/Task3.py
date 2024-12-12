deb7620d225366ee27d9ab37a21eaadffc0efa1b
svenjadunger <svenjadunger@web.de>
2024-12-12 21:39:22 +0100

    feat: add test

--- work_sheet7/Task3.py
+++ work_sheet7/Task3.py
@@ -5,10 +5,12 @@
 # Sheet 7, Task 3
 
 def list_sum(lst):
-    if not lst:
+    if not lst: #liste leer? -> return 0
         return 0
     else:
-        return lst[0] + list_sum(lst[1:]) #1. wert der liste + funktion wird rekursiv m. restl. Liste aufgerufen
+        return lst[0] + list_sum(lst[1:]) #1. wert der liste + funktion wird rekursiv m. restl. Liste aufgerufen (ab 2.Element)
     #summe wird berechnet 1. wert zur summe der restl. liste addiert wird
     
     
+print(list_sum([])) #0
+print(list_sum([1, 2, 3])) #6
