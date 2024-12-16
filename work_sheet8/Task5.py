6e31e6855a65ee05e21e2c49458d9f491005e4a3
svenjadunger <svenjadunger@web.de>
2024-12-16 19:39:25 +0100

    feat: complete task 5

--- work_sheet8/Task5.py
+++ work_sheet8/Task5.py
@@ -3,3 +3,21 @@
 # Group I
 # Matriculation numbers: [827575, 826703, 828610]
 # Sheet 8, Task 5
+
+#ein leeres dic. wird als standardw. verw., um berechn. fibonac-werte zu speichern: memo={}
+#d. schlüssel ist num, d. zugehör. wert is ergebn. der fibonac-berechn.
+def fibonacci(num, memo={}):
+    if num in memo:  # Prüfen, ob wert bereits in dict. gespeich. ist
+        return memo[num] #falls ja, gesp. wert sofort zurückgeb.
+    if num == 0:  # die Fibonaccizahl an position(0) = 0 ist 0
+        return 0
+    if num == 1:  #d. fibonacci-zahl an pos(1) = 1 ist 1
+        return 1
+    #wenn wert no. n. berechn. wurde, wird fib.zahl rekursiv berechn. u. in memo gespeichert:
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+    return memo[num]
+
+print(fibonacci(10))  #55
+print(fibonacci(20))  #6765
+print(fibonacci(40))  #102334155
+
