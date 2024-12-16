2666e8e826ed773ed93b6b0bcc7c840dc31d96c4
svenjadunger <svenjadunger@web.de>
2024-12-16 19:16:09 +0100

    feat: complete task 1

--- work_sheet8/Task1.py
+++ work_sheet8/Task1.py
@@ -3,3 +3,18 @@
 # Group I
 # Matriculation numbers: [827575, 826703, 828610]
 # Sheet 8, Task 1
+
+def lists_to_dict(keys, values):
+    #prüfe ob länge der beiden listen gleich sind
+    if len(keys) != len(values):
+        raise ValueError("Lists must have the same length.") #fehler werfen, wenn nicht gleich lang
+    #die funktion zip verbindet beide listen zu paaren(key, value)
+    #zb ['a', 'b'] und [1, 2] ergeben ('a', 1) und ('b', 2)
+    #dict erstellt daraus ein dictonary
+    return dict(zip(keys, values))
+
+
+keys = ["name", "age", "city"]
+values = ["Alice", 30, "Berlin"]
+result = lists_to_dict(keys, values)
+print(result)  # {'name': 'Alice', 'age': 30, 'city': 'Berlin'}
