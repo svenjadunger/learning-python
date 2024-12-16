71d1a497d8e465071bd875b1c6093e2dee8faa6d
svenjadunger <svenjadunger@web.de>
2024-12-16 19:32:30 +0100

    feat: complete task 4

--- work_sheet8/Task4.py
+++ work_sheet8/Task4.py
@@ -3,3 +3,13 @@
 # Group I
 # Matriculation numbers: [827575, 826703, 828610]
 # Sheet 8, Task 4
+
+def string_to_dict(input_string):
+    return {index: char for index, char in enumerate(input_string)}
+#die funkt. enumerate() fügt jedem element des strings input_string autom. index hinzu
+#zb "abc" → (0, 'a'), (1, 'b'), (2, 'c')
+
+#{index:char for index, char in enumerate(input_string)} erstellt dic. aus index-zeichen-paaren
+
+result = string_to_dict("abc")
+print(result)  #{0: 'a', 1: 'b', 2: 'c'}
